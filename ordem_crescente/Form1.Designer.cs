1117b205cd54553bcba62824dcdd69a2e0933844
jpbertti <joaobertty@gmail.com>
2024-11-13 11:57:34 -0300

    Adicionar arquivos de projeto.

---
+++ ordem_crescente/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ordem_crescente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.lblValor3 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtValor3 = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor1.Location = new System.Drawing.Point(37, 28);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(147, 18);
+            this.lblValor1.TabIndex = 0;
+            this.lblValor1.Text = "Informe um Valor: ";
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor2.Location = new System.Drawing.Point(37, 74);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(147, 18);
+            this.lblValor2.TabIndex = 1;
+            this.lblValor2.Text = "Informe um Valor: ";
+            // 
+            // lblValor3
+            // 
+            this.lblValor3.AutoSize = true;
+            this.lblValor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor3.Location = new System.Drawing.Point(37, 121);
+            this.lblValor3.Name = "lblValor3";
+            this.lblValor3.Size = new System.Drawing.Size(147, 18);
+            this.lblValor3.TabIndex = 2;
+            this.lblValor3.Text = "Informe um Valor: ";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(190, 28);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(171, 20);
+            this.txtValor1.TabIndex = 3;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(190, 75);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(171, 20);
+            this.txtValor2.TabIndex = 4;
+            // 
+            // txtValor3
+            // 
+            this.txtValor3.Location = new System.Drawing.Point(190, 121);
+            this.txtValor3.Name = "txtValor3";
+            this.txtValor3.Size = new System.Drawing.Size(171, 20);
+            this.txtValor3.TabIndex = 5;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(40, 167);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(154, 166);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(286, 167);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 207);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtValor3);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblValor3);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.lblValor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.Label lblValor3;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.TextBox txtValor3;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ ordem_crescente/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ordem_crescente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
+            txtValor3.Clear();
+            txtValor1.Focus();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, valor3;
+
+            valor1 = int.Parse(txtValor1.Text); 
+            valor2 = int.Parse(txtValor2.Text);
+            valor3 = int.Parse(txtValor3.Text);
+
+
+            if (valor1 > valor2 && valor1 > valor3)
+            {
+                if(valor2 > valor3)
+                {
+                    MessageBox.Show("Os números em ordem crescente são: " + valor3 + ", " + valor2 + ", " + valor1);
+                }
+                else 
+                {
+                    MessageBox.Show("Os números em ordem crescente são: " + valor2 + ", " + valor3 + ", " + valor1);
+                }
+            }
+
+            else if (valor2> valor3 && valor2 > valor1)
+            {
+                if(valor3 > valor1)
+                {
+                    MessageBox.Show("Os números em ordem crescente são: " + valor1 + ", " + valor3 + ", " + valor2);
+                }
+                else
+                {
+                    MessageBox.Show("Os números em ordem crescente são: " + valor3 + ", " + valor1 + ", " + valor2);
+                }
+            }
+
+            else if (valor3> valor2 && valor3 > valor1)
+            {
+                if(valor2 > valor1)
+                {
+                    MessageBox.Show("Os números em ordem crescente são: " + valor1 + ", " + valor2 + ", " + valor3);
+                }
+                else
+                {
+                    MessageBox.Show("Os números em ordem crescente são: " + valor2 + ", " + valor1 + ", " + valor3);
+                }
+            }
+        }
+
+    }
+}
